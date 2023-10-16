a6ff08f1273128cc06e28cbf338096e042f96316
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2023-10-16 16:06:20 -0400

    fix: updates
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

---
+++ lib/gcc/x86_64-apple-darwin23/13.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --target=x86_64-apple-darwin23 --with-sysroot=/opt/osxcross-clang-17.0.3-macosx-14.0/target/bin/../SDK/MacOSX14.0.sdk --disable-nls --enable-languages=c,c++,objc,obj-c++ --without-headers --enable-lto --enable-checking=release --disable-libstdcxx-pch --prefix=/opt/osxcross-clang-17.0.3-macosx-14.0/target/bin/.. --with-system-zlib --with-ld=/opt/osxcross-clang-17.0.3-macosx-14.0/target/bin/../bin/x86_64-apple-darwin23-ld --with-as=/opt/osxcross-clang-17.0.3-macosx-14.0/target/bin/../bin/x86_64-apple-darwin23-as --disable-multilib";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "core2" } };
